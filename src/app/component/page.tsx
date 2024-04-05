22ab3c7db26b48ad314a7fb8c8230bcf48dc45fb
adachi_440 <tomoki1999314@gmail.com>
2024-04-06 00:21:10 +0300

    fix next ui

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -7,13 +7,10 @@ export default function Component() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
       <div style={{ marginRight: '20px' }}>
-       
         <YourDeoisits />
-        
       </div>
       <div style={{ marginLeft: '20px' }}>
-      <AccountOperations />
-      
+        <AccountOperations />
       </div>
     </div>
   )
