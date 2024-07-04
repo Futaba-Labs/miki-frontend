5c26da9479564f9ac77384e2ad73bd6b35765f23
adachi_440 <tomoki1999314@gmail.com>
2024-07-04 13:27:29 +0800

    fix mobile header

--- src/components/DepositCard.tsx
+++ src/components/DepositCard.tsx
@@ -54,8 +54,6 @@ export default function DepositCard({ width, height }: DepositCardProps) {
   useEffect(() => {
     const updateDecimalPlaces = () => {
       if (window.innerWidth < 640) {
-        setBalance(roundedNumber(balance, 2))
-      } else if (window.innerWidth < 1024) {
         setBalance(roundedNumber(balance, 4))
       } else {
         setBalance(roundedNumber(balance, 6))
@@ -84,7 +82,6 @@ export default function DepositCard({ width, height }: DepositCardProps) {
             )
           }
           renderValue={(items) => {
-            console.log(items)
             return items.map((item) => (
               <p key={item.key} className='text-green pl-1 font-bold text-lg'>
                 {item.key!.toString()}
@@ -95,7 +92,8 @@ export default function DepositCard({ width, height }: DepositCardProps) {
           {tokens.map((token) => (
             <SelectItem
               key={token.key}
-              value={token.value}
+              textValue={token.value}
+              aria-label={token.value}
               startContent={
                 token.value === 'ETH' ? (
                   <Image src={'/logo/ethereum.svg'} width={15} height={15} alt='ETH' />
